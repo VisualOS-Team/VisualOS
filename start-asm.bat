9ed774e45577585836477a78e02379e7bb95ee29
DALM1 <dimitri.almon@gmail.com>
2024-12-22 02:54:37 +0100

    Init

--- start-asm.bat
+++ start-asm.bat
@@ -1,18 +1,18 @@
+@echo on
 del boot.bin
 del boot.img
 del kernel.bin
 del kernel.o
 
 wsl nasm -f bin boot.asm -o boot.bin
-
 wsl nasm -f bin kernel.asm -o kernel.bin
 
-dd if=/dev/zero of=boot.img bs=512 count=2880
-dd if=boot.bin of=boot.img bs=512 count=1 conv=notrunc
-dd if=kernel.bin of=boot.img bs=512 seek=1 conv=notrunc
+wsl dd if=/dev/zero of=boot.img bs=512 count=2880
+wsl dd if=boot.bin of=boot.img bs=512 count=1 conv=notrunc
+wsl dd if=kernel.bin of=boot.img bs=512 seek=1 conv=notrunc
 
 del boot.bin
 del kernel.bin
 del kernel.o
 
-wsl qemu-system-i386 -drive format=raw,file=boot.img -d int,cpu_reset
+wsl qemu-system-i386 -drive format=raw,file=boot.img -display curses -d int,cpu_reset
--- start-c.bat
+++ start-c.bat
@@ -10,10 +10,10 @@ wsl nasm -f bin loader.asm -o loader.bin
 wsl gcc -m32 -ffreestanding -c kernel.c -o kernel.o
 wsl ld -T linker.ld -o kernel.bin --oformat binary kernel.o
 
-dd if=/dev/zero of=boot.img bs=512 count=2880
-dd if=boot.bin of=boot.img bs=512 count=1 conv=notrunc
-dd if=loader.bin of=boot.img bs=512 count=1 conv=notrunc
-dd if=kernel.bin of=boot.img bs=512 seek=1 conv=notrunc
+wsl dd if=/dev/zero of=boot.img bs=512 count=2880
+wsl dd if=boot.bin of=boot.img bs=512 count=1 conv=notrunc
+wsl dd if=loader.bin of=boot.img bs=512 count=1 conv=notrunc
+wsl dd if=kernel.bin of=boot.img bs=512 seek=1 conv=notrunc
 
 del boot.bin
 del kernel.bin
